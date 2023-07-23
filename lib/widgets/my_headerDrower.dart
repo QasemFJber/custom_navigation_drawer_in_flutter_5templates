73692fb6cb97b418df7832be637c94ce68706350
jaber <jaberqasem75@gmail.com>
2023-07-23 23:38:29 +0300

    build first custom navigation drawer

---
+++ lib/widgets/my_headerDrower.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class MyHeaderDrawer extends StatelessWidget {
+  const MyHeaderDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text('Qasem Jber'),
+        ),
+      ),
+    );
+  }
+}
